e5df827a5b2d554f97fe7f1028fe18163633c2dc
Meital Adivi <ilanadivi47@gmail.com>
2020-08-12 17:53:00 +0300

    update gitignore

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
-
 const cors = require('cors');
-
+const path = require('path');
 const userRouter = require('./api/routes/user');
 const exerciseRouter = require('./api/routes/exercise');
 
 require('./db/mongoose');
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
 app.use(cors());
 app.use(userRouter);
 app.use(exerciseRouter);
+// app.use(express.static(path.join(__dirname, 'client/build')));
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build', 'index.html'));
+});
 
 module.exports = app;
--- db/mongoose.js
+++ db/mongoose.js
@@ -7,8 +7,12 @@ const { MONGO_URI } = process.env;
 
 console.log(MONGO_URI);
 
-mongoose.connect(MONGO_URI,{
+mongoose.connect(MONGO_URI || 'mongodb+srv://ilan:ilan@cluster0-nxx9v.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!!!!');
 });
