c326c9c2f523d542f6b8c9d7503fd7e6b199761f
Meital Adivi <ilanadivi47@gmail.com>
2020-08-13 16:29:33 +0300

    Enable accessing to environment variables on server.js file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const dotenv = require('dotenv'); 
 
-dotenv.config({ path: './config/dev.env' });
+dotenv.config({ path: '.env' });
 
 const port = process.env.PORT || 5000;
 
