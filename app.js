d748301d7bac9cb59c2bf72118df298dfa042440
Meital Adivi <ilanadivi47@gmail.com>
2020-08-30 19:36:12 +0300

    Configure cors origin.

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: `http://localhost:5000`
+    origin: `http://localhost:3000`
 }));
 app.use(userRouter);
 app.use(exerciseRouter);
